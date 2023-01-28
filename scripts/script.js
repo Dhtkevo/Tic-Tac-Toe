25ddf4ee114f3f65535fc537d2b8b2170c9847ff
Kevin <110263079+Dhtkevo@users.noreply.github.com>
2023-01-28 10:41:32 -0500

    Finish general Gui

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,29 @@
-const gameBoard = function () {
-    const board = [];
-}
+const gameBoard = (function () {
+    const body = document.querySelector('body');
+    const guiBoard = document.querySelector('.container');
+    const gameBoard = Array.from(document.querySelectorAll('.cell'));
+    
 
-const players = function () {
+
+    
+})();
+
+
+const Player = function (name, playerTurn, assignedLetter) {
+    let getName = name.toUpperCase();
+    return {getName, playerTurn, assignedLetter};
 
 }
 
 const gameFlow = function () {
-    
+    let x_Text = "X";
+    let o_Text = "O";
+    //Assign X or O to player
+    //Alternate player turns
+    //Cell display X or O When clicked Depending on player 1 or 2
+    //check if three of same letter in row / column/ or diagnal
+    //announce thr winner
+
+    const playerTurn = "one";
+
 }
